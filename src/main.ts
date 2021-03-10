f42a15e135e59f3207046fc18081fd1a4c5c5921
Mobysick <sd.emre.gonen@gmail.com>
2021-03-10 21:46:34 +0300

    Added helmet and cors.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,18 @@ import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { AppLogger } from "./core/logger/logger.service";
+import * as helmet from "helmet";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { logger: false });
-    const logger = await app.resolve(AppLogger);
-    app.useLogger(logger);
+    const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
+    const logger = await app.resolve(AppLogger);
+
+    app.useLogger(logger);
+
+    //security steps
+    app.use(helmet());
+    app.enableCors();
 
     app.useGlobalPipes(
         new ValidationPipe({
