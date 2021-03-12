166a292c48d8ef3825d3635d649811c36cbd9526
Mobysick <sd.emre.gonen@gmail.com>
2021-03-12 18:40:17 +0300

    Fixed typo.

--- src/core/error/exceptions/forbidden.error.ts
+++ src/core/error/exceptions/forbidden.error.ts
@@ -3,7 +3,7 @@ import { ApiErrorMessage } from "../api-error-message";
 
 export class ForbiddenError extends ForbiddenException {
     constructor(message?: ApiErrorMessage) {
-        const errorMessage = message || ApiErrorMessage.FORBBIDDEN;
-        super(errorMessage, ApiErrorMessage.FORBBIDDEN);
+        const errorMessage = message || ApiErrorMessage.FORBIDDEN;
+        super(errorMessage, ApiErrorMessage.FORBIDDEN);
     }
 }
